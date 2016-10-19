370acba32b6d8612ca50ab2576f1c4ad4b90e1f6
Brian Smith <briansmith.8437@gmail.com>
2016-10-19 00:16:28 -0400

    Basic workigns of printf

--- printf.asm
+++ printf.asm
@@ -1,6 +1,70 @@
+section .data
+    teststr db "Hello!", 0
+
 section .text
 
 global _start:
 
+%include "linux.mac"
+
+; strlen(eax) -> ecx
+_strlen:
+    push ebp
+    mov ebp, esp
+
+    push eax
+    push ecx
+    xor ecx, ecx
+
+    strlenloop:
+    cmp byte [eax], 0
+    je strlendone
+
+    inc ecx
+    inc eax
+
+    jmp strlenloop
+    strlendone:
+
+    pop ecx
+    pop eax
+
+    pop ebp
+    ret
+
+_printf:
+    push ebp
+    mov ebp, esp
+
+    push eax
+
+    printfloop:
+
+    cmp byte [eax], 0
+    je printfdone
+
+    cmp byte [eax], 37 ; % is our trigger
+    jne printfprint
+
+    ; We have a special!
+
+
+    ; Print and loop
+    printfprint:
+    push eax
+    sys_write eax, 1 ; write the current char from buffer
+    pop eax
+    inc eax
+    jmp printfloop
+
+    printfdone:
+    pop eax
+
+    pop ebp
+    ret
+
 _start:
-    sys_exit
+    mov eax, teststr
+    call _printf
+
+    sys_exit 0
